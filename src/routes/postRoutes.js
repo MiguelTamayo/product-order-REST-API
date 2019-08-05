ba4b93134a21faa7791171c1c70e9eba34fcfebc
MiguelTamayo <34068757+MiguelTamayo@users.noreply.github.com>
2019-08-04 19:44:42 -0500

    fixed bug that caused header check to be called on all paths in router and updated response for POST /orders

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 const database = require('../databaseConnection/database.js');
 
-//router middleware
-router.use((req, res, next) => {
+//post route middleware
+function headerVerification(req, res, next){
     //check if content header is json
     if( !req.is('application/json') ){
         res.json({error:"'Content-Type' header is not 'application/json'"});
     }else{
         next();
     }
-});
+}
 
-router.post('/products', (req, res) => {
+
+router.post('/products', headerVerification, (req, res) => {
     database.createProduct(req)
         .then(data => {
             res.status(201);
@@ -29,15 +30,20 @@ router.post('/products', (req, res) => {
         });
 });
 
-router.post('/orders', (req, res) => {
+router.post('/orders', headerVerification, (req, res) => {
     database.createOrder(req)
         .then(data => {
             res.status(201);
-            res.send(data);
+            res.json(data);
         })
         .catch(error =>{
-            res.status(404);
-            res.send(error);
+            if(error instanceof Error){
+                res.status(500);
+                res.json({error:"server error"});
+            }else {
+                res.status(400);
+                res.json(error);
+            }
         });
 });
 
